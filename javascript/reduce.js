fea89de628fa10cc82ace20211f9b0e68ad8b503
Safwan <1.safwanashraf@gmail.com>
2026-08-28 16:11:44 +0530

    Practiced reduce problems

---
+++ javascript/reduce.js
@@ -0,0 +1,27 @@
+const array = [10, 50, 20, 80, 30];
+
+// Finding the sum
+const sum = array.reduce((acc, curr) => {
+  return (acc += curr);
+});
+console.log(sum);
+
+// Finding the Maximum
+const max = array.reduce((acc, curr) => {
+  if (acc < curr) {
+    return curr;
+  } else {
+    return acc;
+  }
+});
+console.log(max);
+
+// Use reduce() to find the lowest value in the array. (Do not use Math.min).
+const min = array.reduce((acc, curr) => {
+  if (acc > curr) {
+    return curr;
+  } else {
+    return acc;
+  }
+});
+console.log(min);
