4f3c3623ee25b16ddd0f60e2cf699f5516f00e51
Safwan <1.safwanashraf@gmail.com>
2026-08-28 15:41:52 +0530

    Refine filter JavaScript practice

--- javascript/filter.js
+++ javascript/filter.js
@@ -23,7 +23,7 @@ const primeNumbers = array.filter(isPrime);
 console.log("these are prime numbers: ", primeNumbers);
 
 // You have an array of words. Use filter() to get only the words that have more than 5 letters.
-const fruits = ["apple", "banana", "kiwi", "pineapple", "grape"];
+const words = ["apple", "banana", "kiwi", "pineapple", "grape"];
 
-const bigNames = fruits.filter(fruit => fruit.length > 5);
-console.log(bigNames);
+const longWords = words.filter(word => word.length > 5);
+console.log(longWords);
