a762dc607fb11f6649aca12dc3430aa77c32db59
Safwan <1.safwanashraf@gmail.com>
2026-08-30 02:42:17 +0530

    Worked with promises returning inside async

--- javascript/async-await.js
+++ javascript/async-await.js
@@ -1,7 +1,22 @@
-// Always returns a promise.
+
+// Always returns a promise. Even if we don't return promis manually it will wrap things into a promise object.
 async function getData() {
     return "Safwan Ashraf";
 }
 
 const dataPromise = getData();
+
 dataPromise.then(res => console.log(res));
+
+// Always returns a promise.
+const promise = new Promise((res, rej) => {
+    res("Promise Resolved Value!");
+});
+
+async function getPromiseData() {
+    return promise;
+}
+
+const promiseData = getPromiseData();
+
+promiseData.then(res => console.log(res));
