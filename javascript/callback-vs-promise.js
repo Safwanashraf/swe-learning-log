03ca7aaeee99a31b05ebbd297c171182c4519ce6
Safwan <1.safwanashraf@gmail.com>
2026-08-30 02:26:10 +0530

    Added some callbacks & promises

--- javascript/callback-vs-promise.js
+++ javascript/callback-vs-promise.js
@@ -1,3 +1,5 @@
+const cart = ["pants", "shirts", "tshirts", "kurthas"]
+
 // ❌ The Old Callback Way (Inversion of Control + Nesting)
 createOrder(cart, function (orderId) {
   proceedToPayment(orderId, function (paymentInfo) {
@@ -20,8 +22,14 @@ createOrder(cart, (orderId) => {
 // Each step returns a promise so data flows down the chain
 
 // steps we have orderId, then we need to use proceed to payment, then show order summary, then we need update wallet ballance, then if there's any error for that a catch.
-createOrder(cart)
-    .then(orderId => proceedToPayment(orderId))
+ const promise = createOrder(cart);
+    
+ promise.then(orderId => proceedToPayment(orderId))
     .then(paymentInfo => showOrderSummary(paymentInfo))
     .then(summary => updateWalletBalance(summary))
     .catch(err => console.error("Transaction failed:", err))
+
+function createOrder(cart) {
+    console.log("hi I'm createOrder");
+    return cart = 5;
+}
