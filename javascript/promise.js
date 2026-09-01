dccb63cdc3a0a90b0c329f26d0899acdde619822
Safwan <1.safwanashraf@gmail.com>
2026-09-01 21:40:16 +0530

    Add Promise example

--- javascript/promise.js
+++ javascript/promise.js
@@ -1,1 +1,23 @@
-// Let's write some promise here.
+// A Promise represents a result that will be available later.
+const myPromise = new Promise((resolve, reject) => {
+  const isSuccessful = true;
+
+  setTimeout(() => {
+    if (isSuccessful) {
+      resolve("Task completed successfully!");
+    } else {
+      reject(new Error("Task failed."));
+    }
+  }, 1000);
+});
+
+myPromise
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  })
+  .finally(() => {
+    console.log("Promise settled.");
+  });
