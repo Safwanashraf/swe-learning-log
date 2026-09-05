49643508dc766a35d626ee00da9c63759ecb96e8
Safwan <1.safwanashraf@gmail.com>
2026-09-05 23:09:13 +0530

    Add async await fetch example

--- javascript/async-await-advance.js
+++ javascript/async-await-advance.js
@@ -4,4 +4,18 @@ const p = new promise((resolve, reject) => {
     }, 3000);
 })
 
-//let's do this fully each stages.
+// The Older .then() Way (Callback/Promise Chain)
+function getData() {
+  fetch("https://api.github.com/users/safwanashraf")
+    .then(res => res.json())
+    .then(data => console.log(data));
+  console.log("Namaste JavaScript"); // Prints FIRST!
+}
+
+// The Modern async/await Way
+async function getData() {
+  const res = await fetch("https://api.github.com/users/safwanashraf");
+  const data = await res.json();
+  console.log(data);
+  console.log("Namaste JavaScript"); // Prints LAST!
+}
